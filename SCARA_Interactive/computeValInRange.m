66619955f83265c9cf1bd82c5c6cb2517703101e
Chase <54785464+cnoren@users.noreply.github.com>
2021-02-23 21:37:41 -0500

    Made a quick change to computeValInRange
    Added functionality to Val in Range to allow for the calculation of the center value of the interval (for initialization purposes for the estimates)

--- SCARA_Interactive/computeValInRange.m
+++ SCARA_Interactive/computeValInRange.m
@@ -1,4 +1,4 @@
-function [valInRange] = computeValInRange(LB,UB, seed)
+function [valInRange] = computeValInRange(LB,UB, seed, half)
 %computeValInRange: This function computes a value in the range listed by
 %lower bound and upper bound:
 %INPUTS: 
@@ -6,6 +6,10 @@ function [valInRange] = computeValInRange(LB,UB, seed)
 %    UB: This is the upper bound that you want on the value
 %    seed: this is the seed in the random number generator
 seed;
-valInRange = (UB-LB).*rand(1,1) + LB;
+if half
+    valInRange = (LB + UB)/2;
+else
+    valInRange = (UB-LB).*rand(1,1) + LB;
+end % This just checks to see if we want the middle of the interval
 end
 
